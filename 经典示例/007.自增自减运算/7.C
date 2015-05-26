e97e5412bcdd8ffb3ce0f13dcc0feb3d8a00a016
zxgblade <15196612376@163.com>
2015-05-26 17:25:56 +0800

    Update 7.C

--- 经典示例/007.自增自减运算/7.C
+++ 经典示例/007.自增自减运算/7.C
@@ -6,13 +6,13 @@ main()
 	b=a++;
 	c=++b;
 
-	printf("a = %d, b = %d, c = %d\n",a,b,c);
-	printf("i,i++,i++ = %d,%d,%d\n",i,i++,i++);
-	printf("%d\n",++i);
-	printf("%d\n",--i);
-	printf("%d\n",i++);
-	printf("%d\n",i--);
-	printf("%d\n",-i++);
-	printf("%d\n",-i--);
+	printf("a = %d, b = %d, c = %d\n",a,b,c);       /* 6,6,6 */
+	printf("i,i++,i++ = %d,%d,%d\n",i,i++,i++);     /* 10,10,11*/
+	printf("%d\n",++i); // 13
+	printf("%d\n",--i); // 12
+	printf("%d\n",i++); //12
+	printf("%d\n",i--); //12
+	printf("%d\n",-i++); //这种考运算符的优先级，显示负号，然后是自增，所以是 -12，结果i变为-11
+	printf("%d\n",-i--); // 11，最后i为10
 	getchar();
 }
